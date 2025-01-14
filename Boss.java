94eefe59aa2a2a837f48990bd91583fb78da9e5b
Seth Fenton <sfenton50@stuy.edu>
2025-01-13 22:01:49 -0500

    Lich has been vaguely coded but not a ton, have to go through and finish functions

--- Boss.java
+++ Boss.java
@@ -1,26 +1,84 @@
 public class Boss extends Adventurer{
-  int caffeine, caffeineMax;
-  String preferredLanguage;
+//have to code the rest of this 
+  int necropoints, maxNecromagic;
+  String elimQuote;
 
-  /*the other constructors ultimately call the constructor
-  *with all parameters.*/
-  public Boss(String name, int hp, String language){
+
+ public Boss(String name, int hp, String elimQuote){
     super(name,hp);
-    caffeineMax = 12;
-    caffeine = caffeineMax/2;
-    preferredLanguage = language;
+    maxCharge = 8;
+    charge = maxCharge/2;
+    elimQuote = elimQuote;
   }
 
   public Boss(String name, int hp){
-    this(name,hp,"c++");
+    this(name,hp,"I am a god to you pathetic mortals");
   }
 
   public Boss(String name){
-    this(name,24);
+    this(name,20 + (int) (Math.random() * 9));
   }
 
   public Boss(){
-    this("Carmack");
+    this("Vecna");
   }
+
+  /*The next 8 methods are all required because they are abstract:*/
+  public String getSpecialName(){
+    return "charge";
+  }
+
+  public int getSpecial(){
+    return charge;
+  }
+
+  public void setSpecial(int n){
+    charge = n;
+  }
+
+  public int getSpecialMax(){
+    return maxCharge;
+  }
+
+  public String attack(Adventurer other){
+    int damage = (int)((Math.random()*5) + 2);
+    other.applyDamage(damage);
+    restoreSpecial(2);
+    return this + " fired a laser beam at "+ other + " and dealt "+ damage +
+    " points of damage. They then let out a horrifying grin.";
+  }
+
+
+
+  public String specialAttack(Adventurer other){
+    if(getSpecial() >= 8){
+      setSpecial(getSpecial()-8);
+      int damage = 3 + (int) (2 * Math.random()) * getAttackMultiplier();
+      other.applyDamage(damage);
+
+      return this + " used their laser barrage dealing " + damage + " points of damage and singing their enemies";
+
+    }else{
+      return "Not enough energy to use the ultimate code. Instead "+attack(other);
+    }
+
+  }
+
+
+  /*Restores 5 special to other*/
+  public String support(Adventurer other){
+    return getName() + " says kill them for me "+other+" and buffs his attack substantially "
+
+  }
+  /*Restores 6 special and 1 hp to self.*/
+  public String support(){
+
+    return getName() +" says fine I'll do it myself and significantly boosts their own attack power.";
+  }
+
+
+
+
+
 
 }
